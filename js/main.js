d48ab9235e7f3b85c54345d335cc5df1540412fe
Parker <parker.caskey@gmail.com>
2020-04-14 09:50:20 -0600

    continued dev. see notes
    added my profile image.
    built profile section.
    added JS to set hero height
    added scroll down chevron

--- js/main.js
+++ js/main.js
@@ -1,7 +1,30 @@
 $(document).ready(function () {
+    init();
+});
+
+function init() {
+    
+    setHeroHeight();
+
+    setWaypoints();
+}
+
+function setWaypoints() {
     $('.wp1').waypoint(function () {
         $('.wp1').addClass('animated fadeIn');
     }, {
         offset: '75%'
     });
-});
+}
+
+function setHeroHeight()
+{
+    let newHeight = $(window).height();
+    $('.hero').css({ height:  newHeight + 'px' });
+
+	$(window).on('resize', function() 
+	{  	
+        let newHeight = $(window).height(); 
+		$('.hero').css({ height: newHeight + 'px' });
+	}); 
+}
